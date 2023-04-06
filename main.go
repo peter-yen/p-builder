5ed924e4a8bfdf2ff8b06628de82c0d0bb296230
spacepi-peter <h78823462@gmail.com>
2023-04-06 18:07:28 +0800

    no message

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
-package p_builder
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
 
 func main() {
 
+	tmp, err := template.New("test").Parse("{{define \"T\"}}Hello, {{.}}!{{end}}")
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	if err = tmp.Execute(os.Stdout, "World"); err != nil {
+		fmt.Print(err)
+		return
+	}
 }
